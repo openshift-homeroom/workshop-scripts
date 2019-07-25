a9b2f042a923d1bdde3dd4c99213f96af4d4f030
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2019-07-25 16:17:47 +1000

    Split out default settings so can be read separately.

--- setup-environment.sh
+++ setup-environment.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 fail()
 {
     echo "Error:" $* 1>&2
@@ -11,8 +9,6 @@ warn()
     echo "Warning:" $* 1>&2
 }
 
-echo "### Reading the workshop configuation."
-
 SCRIPTS_DIR=`dirname $0`
 WORKSHOP_DIR=`dirname $SCRIPTS_DIR`
 SOURCE_DIR=`dirname $WORKSHOP_DIR`
@@ -23,6 +19,12 @@ if [ `basename $WORKSHOP_DIR` != ".workshop" ]; then
     fail "Failed to find workshop directory."
     exit 1
 fi
+
+echo "### Reading the default configuation."
+
+. $SCRIPTS_DIR/default-settings.sh
+
+echo "### Reading the workshop configuation."
 
 if [ ! -f $WORKSHOP_DIR/settings.sh ]; then
     warn "Cannot find any workshop settings."
@@ -37,22 +39,7 @@ WORKSHOP_NAME=${WORKSHOP_NAME:-$REPOSITORY_NAME}
 SPAWNER_APPLICATION=${SPAWNER_APPLICATION:-$WORKSHOP_NAME}
 DASHBOARD_APPLICATION=${DASHBOARD_APPLICATION:-$WORKSHOP_NAME}
 
-SPAWNER_REPO=${SPAWNER_REPO:-openshift-labs/workshop-spawner}
-SPAWNER_VERSION=${SPAWNER_VERSION:-4.3.0}
-SPAWNER_MODE=${SPAWNER_MODE:-learning-portal}
-SPAWNER_VARIANT=${SPAWNER_VARIANT:-production}
-
-DASHBOARD_REPO=${DASHBOARD_REPO:-openshift-labs/workshop-dashboard}
-DASHBOARD_VERSION=3.6.2
-DASHBOARD_IMAGE=quay.io/openshiftlabs/workshop-dashboard:$DASHBOARD_VERSION
-DASHBOARD_VARIANT=${DASHBOARD_VARIANT:-production}
-
 WORKSHOP_IMAGE=${WORKSHOP_IMAGE:-$DASHBOARD_IMAGE}
-
-RESOURCE_BUDGET=${RESOURCE_BUDGET:-medium}
-MAX_SESSION_AGE=${MAX_SESSION_AGE:-3600}
-IDLE_TIMEOUT=${IDLE_TIMEOUT:-300}
-LETS_ENCRYPT=${LETS_ENCRYPT:-false}
 
 VERSION_INFO=`oc get --raw /version 2>/dev/null`
 
