05b5684ed996168c754f2b3178e2702cd6c6ec35
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2019-07-26 09:26:59 +1000

    Add deployment mode for personal deployment.

--- default-settings.sh
+++ default-settings.sh
@@ -5,6 +5,7 @@ SPAWNER_VARIANT=production
 DASHBOARD_REPO=openshift-labs/workshop-dashboard
 DASHBOARD_VERSION=3.6.2
 DASHBOARD_IMAGE=quay.io/openshiftlabs/workshop-dashboard:3.6.2
+DASHBOARD_MODE=
 DASHBOARD_VARIANT=production
 RESOURCE_BUDGET=medium
 MAX_SESSION_AGE=3600
