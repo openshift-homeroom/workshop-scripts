d754ca452c67c0af21c6d9c1c17f44131dbeada5
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2019-09-05 12:41:35 +1000

    Update to version 6.0.1 of spawner.

--- default-settings.sh
+++ default-settings.sh
@@ -1,10 +1,10 @@
 TERMINAL_IMAGE=quay.io/openshifthomeroom/workshop-terminal:3.0.0
 DASHBOARD_IMAGE=quay.io/openshifthomeroom/workshop-dashboard:4.1.0
-SPAWNER_IMAGE=quay.io/openshifthomeroom/workshop-spawner:6.0.0
+SPAWNER_IMAGE=quay.io/openshifthomeroom/workshop-spawner:6.0.1
 CONSOLE_IMAGE=quay.io/openshift/origin-console:4.1
 
 SPAWNER_REPO=openshift-homeroom/workshop-spawner
-SPAWNER_VERSION=6.0.0
+SPAWNER_VERSION=6.0.1
 SPAWNER_MODE=learning-portal
 SPAWNER_VARIANT=production
 SPAWNER_ROLE=admin
