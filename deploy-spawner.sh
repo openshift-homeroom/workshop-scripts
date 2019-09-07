fd3474761660afe3e338067a00f00f5591e44105
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2019-09-07 16:24:32 +1000

    Support pre pulling images when deploying spawner.

--- deploy-spawner.sh
+++ deploy-spawner.sh
@@ -52,6 +52,69 @@ if [[ "$SPAWNER_MODE" =~ ^(hosted-workshop|terminal-server)$ ]]; then
     fi
 fi
 
+if [ x"$PREPULL_IMAGES" == x"true" ]; then
+    echo "### Deploy daemon set to pre-pull images."
+
+    cat << EOF | oc apply -n "$PROJECT_NAME" -f -
+apiVersion: apps/v1
+kind: DaemonSet
+metadata:
+  name: $WORKSHOP_NAME-cache
+spec:
+  selector:
+    matchLabels:
+      app: $WORKSHOP_NAME-cache
+  template:
+    metadata:
+      labels:
+        app: $WORKSHOP_NAME-cache
+    spec:
+      initContainers:
+      - name: prepull-spawner 
+        image: $SPAWNER_IMAGE
+        command: ["/bin/true"]
+        resources:
+          limits:
+            memory: 128Mi
+      - name: prepull-terminal 
+        image: $TERMINAL_IMAGE
+        command: ["/bin/true"]
+        resources:
+          limits:
+            memory: 128Mi
+          requests:
+            memory: 128Mi
+      - name: prepull-workshop 
+        image: $WORKSHOP_IMAGE
+        command: ["/bin/true"]
+        resources:
+          limits:
+            memory: 128Mi
+          requests:
+            memory: 128Mi
+      - name: prepull-console
+        image: $CONSOLE_IMAGE
+        command: ["/bin/true"]
+        resources:
+          limits:
+            memory: 128Mi
+          requests:
+            memory: 128Mi
+      containers:
+      - name: pause
+        image: gcr.io/google_containers/pause
+        resources:
+          limits:
+            memory: 128Mi
+          requests:
+            memory: 128Mi
+EOF
+
+    if [ "$?" != "0" ]; then
+        warn "Creation of daemonset to pre-pull images failed."
+    fi
+fi
+
 echo "### Creating spawner application."
 
 TEMPLATE_ARGS=""
