2b605a43dcfdf97a8f2b4f5af0c3e284df9a6708
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2019-10-15 11:51:50 +1100

    Delete spawner resources created from template.

--- delete-resources.sh
+++ delete-resources.sh
@@ -4,8 +4,16 @@ SCRIPTS_DIR=`dirname $0`
 
 . $SCRIPTS_DIR/setup-environment.sh
 
-echo "### Delete extra resources."
+echo "### Delete spawner resources."
 
 if [ -d $WORKSHOP_DIR/resources/ ]; then
     oc delete -f $WORKSHOP_DIR/resources/ --recursive
+fi
+
+if [ -f $WORKSHOP_DIR/templates/spawner-resources.yaml ]; then
+    oc process \
+        -f $WORKSHOP_DIR/templates/spawner-resources.yaml \
+        --param SPAWNER_APPLICATION="$SPAWNER_APPLICATION" \
+        --param SPAWNER_NAMESPACE="$PROJECT_NAME" | \
+        oc delete -f -
 fi
